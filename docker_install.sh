ab0b106c7b81b4cf235bfabad28b6b2c751368ce
Stephen Deane <wildmechanic@gmail.com>
2022-12-28 02:21:18 -0700

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -7,7 +7,7 @@ test "$UID" -gt 0 || { info "don't run this as root!"; exit; }
 info "setting / verifying sudo timestamp"
 sudo -v
 
-sudo apt install ca-certificates curl gnupg lsb-release -y
+sudo apt install -y ca-certificates curl gnupg lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -15,7 +15,15 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt install -y docker-ce-rootless-extras
+
+sudo apt install uidmap
+
+dockerd-rootless-setuptool.sh install
+
+
