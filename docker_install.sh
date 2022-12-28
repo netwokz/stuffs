75daf413e99bcaf57c3e89e5d80e6a0113e8f0e8
Stephen Deane <wildmechanic@gmail.com>
2022-12-28 01:45:55 -0700

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -9,11 +9,11 @@ sudo -v
 
 sudo apt install ca-certificates curl gnupg lsb-release -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 
-apt update -y
-apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 
-curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
