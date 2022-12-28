91407e3e783cf882dd9365c3808c20217cdc3755
Stephen Deane <wildmechanic@gmail.com>
2022-12-28 02:14:21 -0700

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -14,8 +14,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
+sudo chmod +x /usr/local/bin/docker-compose
